9708f4fc4d495fd3a5522afb041ba84664b2fffa
Valentin Teutschbein <mail@valteu.com>
2024-04-02 18:25:21 +0200

    added backend sample api request for frontend

--- server/main.py
+++ server/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import items, users
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(items.router)
 app.include_router(users.router)
