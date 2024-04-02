e90996ad410ea57272b063d5829e2a8fb8b888ea
hoischreck <hoischreck@gmx.de>
2024-04-02 18:12:13 +0200

    added Dockerfile

--- start_client.py
+++ start_client.py
@@ -1,18 +1,4 @@
-import os
-import subprocess
-import sys
-
-def run_command(command, cwd=None):
-    """
-    Run a shell command in a specified directory and print the output.
-    """
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd)
-    while True:
-        output = process.stdout.readline()
-        if not output and process.poll() is not None:
-            break
-        if output:
-            print(output.strip().decode())
+from _utilities import *
             
 def start_client(path=os.path.join(os.getcwd(), "client")):
     run_command(f'cd {path} && npm start')
--- start_server.py
+++ start_server.py
@@ -1,18 +1,4 @@
-import os
-import subprocess
-import sys
-
-def run_command(command, cwd=None):
-    """
-    Run a shell command in a specified directory and print the output.
-    """
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd)
-    while True:
-        output = process.stdout.readline()
-        if not output and process.poll() is not None:
-            break
-        if output:
-            print(output.strip().decode())
+from _utilities import *
             
 def start_server(path=os.path.join(os.getcwd(), "server")):
 
@@ -23,10 +9,10 @@ def start_server(path=os.path.join(os.getcwd(), "server")):
     print("Activating the virtual environment")
     if sys.platform == "win32":
         # Windows requires a different command to activate the virtual environment
-        run_command(f'cd {path} && .\\venv\\Scripts\\activate && uvicorn main:app')
+        run_command(f'cd {path} && .\\venv\\Scripts\\activate && uvicorn main:app --port=3001')
     else:
         # Linux and macOS use this command
-        run_command(f'cd {path} && . venv/bin/activate && uvicorn main:app')
+        run_command(f'cd {path} && . venv/bin/activate && uvicorn main:app --port=3001')
 
 if __name__ == "__main__":
     start_server()
