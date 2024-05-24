321d4a5f1760ffb2307309df2392f1506a4d5d32
valteu <mail@valteu.com>
2024-05-24 16:05:38 +0200

    user removal working fr

--- userrem.sh
+++ userrem.sh
@@ -3,12 +3,13 @@
 
 dirname="dir$1"
 
+
 [ -d /userdirs/$dirname ] && rm -rf /userdirs/$dirname
 
 smbpasswd -x $1
 userdel -r $1
 
-sed -i "/#dirname#/,/#end#/d" /etc/samba/smb.conf
+sed -i "/#$dirname#/,/#end#/d" /etc/samba/smb.conf
 
 smbcontrol smbd reload-config
 
