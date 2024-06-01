5334cd4d3035aa6907cddb0eb60a6553cfdfc6f6
Jannis Jost <jannis.jost@student.hpi.uni-potsdam.de>
2024-06-01 15:05:45 +0200

    changed pocketbase setting

---
+++ pocketbase/pb_migrations/1717246835_updated_files.js
@@ -0,0 +1,52 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("klsy3g4z1qtbox1")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "xjugtgso",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vplmeukd",
+    "name": "field",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("klsy3g4z1qtbox1")
+
+  // remove
+  collection.schema.removeField("xjugtgso")
+
+  // remove
+  collection.schema.removeField("vplmeukd")
+
+  return dao.saveCollection(collection)
+})
